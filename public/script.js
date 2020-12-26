981d6c2c0ea33a4f991b041143750776bb67e456
aryan anwar <aryananwar221@gmail.com>
2020-12-26 06:05:32 -0500

    hiii
    simple socket setup,, basic self camera, needs work on showing both users cameras to each other, needs ui and stuff

---
+++ public/script.js
@@ -0,0 +1,51 @@
+const socket = io('/')
+const videoGrid = document.getElementById("video-grid")
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    vidStream(myVideo, stream)
+
+    myPeer.on('call', call => {
+        call.answer(stream)
+    })
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+    })
+})
+ 
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id)
+})
+
+
+socket.on('user-connected', userId => {
+    console.log("User connected: " + userId)
+})
+
+function vidStream(video, stream)
+{
+    video.srcObject = stream
+    video.play()
+    videoGrid.append(video)
+}
+
+function connectToNewUser(userId, stream)
+{
+    const call = myPeer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream =>{
+        addVideoStream(userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+}
